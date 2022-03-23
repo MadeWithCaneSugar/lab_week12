fda713e303b8dd0332d38dd3498f470b672a55c3
MitchelW <mwebb40@my.bcit.ca>
2022-03-23 14:53:51 -0700

    Adapting part 3 of last lab to use function

--- Lab-Week-12/src/app.js
+++ Lab-Week-12/src/app.js
@@ -1,4 +1,4 @@
-//part 1
+//part 2
 function elementSelector (x)   {
     let selection = document.querySelector(x)
     return selection
@@ -6,7 +6,7 @@ function elementSelector (x)   {
 console.log(elementSelector(/*insert html element between quotes here*/))
 
 // Alternatively, if I'm to follow your words exactly;
-// "... a function that selects a single element and logs it to the console"
+// "...a function that selects a single element and logs it to the console"
 // I would create a different function: A function that includes the log
 // inside the function instead of requiring it later on. 
 // That alternative function is below:
@@ -20,3 +20,31 @@ function elementSelectorAlt (y) {
 // of the selection that can be called later. That can be easily changed
 // by re-adding the "let selectionAlt = document.querySelector(y)", and
 // then altering the return line to say "return selectionAlt"
+
+//part3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+function honorsChecker (x)  {
+    const studentsScholarship = []
+    for (let item of x)   {
+        let gradeSum = 0
+        for (let grade of item.grades)    {
+        gradeSum += grade
+        }
+        let numOfGrades = item.grades.length
+        let avgGrade = gradeSum / numOfGrades
+        if (avgGrade >= 80)   {
+            studentsScholarship.push(
+                {"name": item.name, "gradeAvg": avgGrade})
+        
+        }
+    }
+    return(studentsScholarship)
+}
+  
+console.log(honorsChecker(students))
